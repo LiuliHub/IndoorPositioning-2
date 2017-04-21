8f081b174aa1c02bcdf4ddb827ec40fc3741f42b
Pere <pere.pala@gmail.com>
2017-04-21 10:18:42 +0200

    modificacions ahir

--- bases.py
+++ bases.py
@@ -1,12 +1,28 @@
+#f of camera
+f=1
+
 #Camera coordinates with respect to the world
 c=np.array([[1],[1],[10]])
 
 #Point coordinates
 v=np.array([[2],[2],[1]])
 
-#Rotation
-R=np.array([[-1, 0, 0],[0, 1, 0],[0, 0, -1]])
+d2r=pi/180
 
+#Arbitrary rotation on x axis
+thx=90*d2r
+Rx=np.array([[1, 0, 0],
+             [0, np.cos(thx), -np.sin(thx)],
+             [0, np.sin(thx),  np.cos(thx)]])
+
+#Arbitrary rotation on y axis
+thy=90*d2r
+Ry=np.array([[np.cos(thy), 0, np.sin(thy)],
+             [0, 1, 0],
+             [-np.sin(thy), 0, np.cos(thy)]])
+
+
+R=Ry.dot(Rx)
 # v-c :final vector minus initial vector)
 #      This is the point seen from the camera coordinates
 # without rotation
