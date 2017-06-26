cf5010796005fccbdfbda24db824d26399149a84
Pere <pere.pala@gmail.com>
2017-06-26 18:26:35 +0200

    modificacions

--- bases.py
+++ bases.py
@@ -1,3 +1,16 @@
+import numpy as np
+"""
+Given a set of points p1..p4
+Find how they are seen by a camera
+
+The camera is situated at c
+The camera is first rotated thx around the world x axis
+Then it is rotated thy around the world (not the local) y axis
+Then it is rotated thz around the world z axis
+
+The focal length of the camera is f
+"""
+
 #f of camera
 f=1
 
@@ -5,12 +18,19 @@ f=1
 c=np.array([[1],[1],[10]])
 
 #Point coordinates
-v=np.array([[2],[2],[1]])
+#v=np.array([[0],[0],[0]])
+p1=np.array([0,0,0]);
+p2=np.array([40,0,0]);
+p3=np.array([40,100,0]);
+p4=np.array([0,100,0]);
+v=np.vstack((p1,p2,p3,p4));
+v=v.T;
 
-d2r=pi/180
+d2r=np.pi/180
 
-#Arbitrary rotation on x axis
-thx=90*d2r
+#Arbitrary rotation of camera on x axis. 
+#Axes are always WORLD axes. They do not change with the object
+thx=180*d2r
 Rx=np.array([[1, 0, 0],
              [0, np.cos(thx), -np.sin(thx)],
              [0, np.sin(thx),  np.cos(thx)]])
@@ -22,7 +42,7 @@ Ry=np.array([[np.cos(thy), 0, np.sin(thy)],
              [-np.sin(thy), 0, np.cos(thy)]])
 
 #Arbitrary rotation on z axis
-thz=90*d2r
+thz=85*d2r
 Rz=np.array([[np.cos(thz), -np.sin(thz), 0],
              [np.sin(thz),  np.cos(thz), 0],
              [0, 0, 1]])
